b3ec8c165877d0bd3d6fdcf0cb9e453eedb8babc
Pablo Bernal Modino <pablobernal_92@hotmail.com>
2018-12-11 22:43:33 +0100

    First commit

---
+++ LanPlayGui/MainForm.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace LanPlayGui
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(162, 199);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.ListBox1_SelectedValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(3, 159);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.93814F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.06186F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.96837F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 207F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(351, 233);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(171, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Switch configuration";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Servers";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(171, 29);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.58491F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.41509F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(177, 201);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.label8, 1, 2);
+            this.tableLayoutPanel3.Controls.Add(this.label7, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.label6, 1, 1);
+            this.tableLayoutPanel3.Controls.Add(this.label5, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.label4, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 0);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(171, 94);
+            this.tableLayoutPanel3.TabIndex = 2;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label8.Location = new System.Drawing.Point(89, 63);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 30);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "10.13.37.1";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label7.Location = new System.Drawing.Point(4, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 30);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Gateway";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Location = new System.Drawing.Point(89, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 30);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "255.255.0.0";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(4, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 30);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Subnet Mask";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(89, 1);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 30);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "10.13.?.?";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(4, 1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 30);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "IP Address";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 250);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(375, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 272);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "LanPlayGUI";
+            this.Load += new System.EventHandler(this.Form1_LoadAsync);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ LanPlayGui/MainForm.cs
@@ -0,0 +1,216 @@
+﻿using LanPlayGui.Extensions;
+using LanPlayGui.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LanPlayGui
+{
+    public partial class MainForm : Form
+    {
+        private const string serverListFileName = "serverlist.txt";
+        private const string releasesUrl = "https://api.github.com/repos/spacemeowx2/switch-lan-play/releases";
+
+        private static string executableName = Environment.Is64BitOperatingSystem ? "lan-play-win64.exe" : "lan-play-win32.exe";
+        private static HttpClient httpClient = new HttpClient();
+
+        private IEnumerable<Uri> serverList = new List<Uri>();
+        private Uri currentServer = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_LoadAsync(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            toolStripStatusLabel1.Text = "Checking for LanPlay updates...";
+
+            IList<Uri> servers = new List<Uri>();
+            using (TextReader reader = File.OpenText(serverListFileName))
+            {
+                string line;
+                while((line = await reader.ReadLineAsync()) != null)
+                {
+                    if (!line.StartsWith("http://", StringComparison.OrdinalIgnoreCase))
+                        line = "http://" + line;
+                    servers.Add(new Uri(line));
+                }
+                    
+            }
+            serverList = servers.OrderBy(u => u.Host);
+            bindingSource1.DataSource = serverList;
+
+            listBox1.DataSource = bindingSource1;
+            listBox1.DisplayMember = "Host";
+
+            Release release = await GetLatestReleaseAsync();
+            if (!File.Exists(executableName))
+            {
+                toolStripStatusLabel1.Text = "Downloading LanPlay...";
+                if(!await DownloadLanPlayExecutable(release))
+                {
+                    toolStripStatusLabel1.Text = executableName + " not found";
+                    return;
+                }
+            }
+            else
+            {
+                if (release == null)
+                {
+                    toolStripStatusLabel1.Text = "Error checking for LanPlay updates";
+                }
+                else if (await CheckUpdate(release))
+                {
+                    DialogResult result = MessageBox.Show("An update is available. Do you want to download it?", "Update available", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        toolStripStatusLabel1.Text = "Updating LanPlay...";
+                        if (!await DownloadLanPlayExecutable(release))
+                        {
+                            toolStripStatusLabel1.Text = "Error downloading LanPlay update";
+                            return;
+                        }
+                    }
+                    toolStripStatusLabel1.Text = "Ready";
+                }               
+            }
+
+            button1.Enabled = true;
+        }
+
+        private void ListBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            currentServer = (Uri)listBox1.SelectedItem;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (currentServer == null)
+                return;
+
+            Process.Start(executableName, "--relay-server-addr " + currentServer.AbsoluteUri);
+        }
+
+        private async Task<Release> GetLatestReleaseAsync()
+        {
+            IEnumerable<Release> releases = new List<Release>();
+
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("LanPlayGui");
+            try
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(releasesUrl))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string resultString = await response.Content.ReadAsStringAsync();
+                        releases = JsonConvert.DeserializeObject<IEnumerable<Release>>(resultString);
+                    }
+                }
+                return releases.FirstOrDefault();
+            }
+            catch (HttpRequestException)
+            {
+                return null;
+            }
+
+        }
+
+        private async Task<bool> DownloadLanPlayExecutable(Release release)
+        {
+            if (release == null)
+                return false;
+            Asset executableAsset = release.Assets.FirstOrDefault(a => a.Name == executableName);
+            if (executableAsset == null)
+                return false;
+
+            try
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(executableAsset.BrowserDownloadUrl))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        await response.Content.ReadAsFileAsync(executableName, true);
+                    }
+                }
+                return true;
+            }
+            catch (HttpRequestException)
+            {
+                return false;
+            }
+        }
+
+        private async Task<bool> CheckUpdate(Release release)
+        {
+            if (release == null)
+                return false;
+            var checkSumAsset = release.Assets.FirstOrDefault(a => a.Name == "sha1sum.txt");
+
+            if (checkSumAsset == null)
+                return false;
+
+            string responseText = null;
+            try
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(checkSumAsset.BrowserDownloadUrl))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        responseText = await response.Content.ReadAsStringAsync();
+                    }
+                }
+            }
+            catch (HttpRequestException)
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(responseText))
+                return false;
+
+            IEnumerable<string> checkSumList = responseText.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            string checkSum = checkSumList.Where(s => s.Contains(executableName))
+                .Select(s => s.Split(' ').FirstOrDefault()).FirstOrDefault();
+
+            if (string.IsNullOrEmpty(checkSum))
+                return false;
+
+            return !ComputeFileCheckSum(executableName).Equals(checkSum, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private string ComputeFileCheckSum(string filePath)
+        {
+            StringBuilder formatted = null;
+            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            using (BufferedStream bs = new BufferedStream(fs))
+            {
+                using (SHA1Managed sha1 = new SHA1Managed())
+                {
+                    byte[] hash = sha1.ComputeHash(bs);
+                    formatted = new StringBuilder(2 * hash.Length);
+                    foreach (byte b in hash)
+                    {
+                        formatted.AppendFormat("{0:X2}", b);
+                    }
+                }
+            }
+
+            return formatted?.ToString();
+        }
+    }
+}
